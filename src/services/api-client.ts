fe64d626ff894e66caf73ff75a5b23758877ca6e
Kevin Schweizerhof <kevin.schweizerhof@reishauer.com>
2023-11-14 14:06:55 +0100

    Custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "08f5abd98d26412497ba6bcbcdf198a8"
+        key: "fb17f3fc58354f76bae744d5255a399b"
     }
 })
