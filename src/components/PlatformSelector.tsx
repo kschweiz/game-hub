24e756cd9b48d6b7f3d575a280e2c1bcd9298740
Kevin Schweizerhof <kevin.schweizerhof@reishauer.com>
2023-11-15 11:13:25 +0100

    Build platform selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,34 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import { useState } from "react";
+import { Platform } from "../hooks/useGames";
+
+function PlatformSelector() {
+  const { data, error } = usePlatforms();
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
+
+  if (error) return null;
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform ? selectedPlatform.name : "Platforms"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            key={platform.id}
+            onClick={() => setSelectedPlatform(platform)}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+}
+
+export default PlatformSelector;
