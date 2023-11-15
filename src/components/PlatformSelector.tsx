dea770fdb95bf37c70091f35b1c829d47b99dd58
Kevin Schweizerhof <kevin.schweizerhof@reishauer.com>
2023-11-15 11:42:10 +0100

    Filtering games by platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,27 +1,28 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
-import { useState } from "react";
 import { Platform } from "../hooks/useGames";
 
-function PlatformSelector() {
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+function PlatformSelector({ onSelectPlatform, selectedPlatform }: Props) {
   const { data, error } = usePlatforms();
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
-    null
-  );
 
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform ? selectedPlatform.name : "Platforms"}
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
           <MenuItem
+            onClick={() => onSelectPlatform(platform)}
             key={platform.id}
-            onClick={() => setSelectedPlatform(platform)}
           >
             {platform.name}
           </MenuItem>
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,9 @@
 import useData from "./useData";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
