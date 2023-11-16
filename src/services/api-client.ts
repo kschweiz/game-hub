f4f66e8ef2ed97cb5f41c6e8347da9b8390e61ce
Kevin Schweizerhof <kevin.schweizerhof@reishauer.com>
2023-11-16 10:48:18 +0100

    Remove useData, useQuery for Games and Platforms.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "fb17f3fc58354f76bae744d5255a399b"
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "fb17f3fc58354f76bae744d5255a399b",
+  },
+});
